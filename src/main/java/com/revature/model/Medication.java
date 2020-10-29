bdddb24796644a4ff58ab05129efc874f0ab86b9
Rabinson Shrestha <rabinshrestha4100@gmail.com>
2020-10-29 00:25:53 -0500

    lets gooooooooooo

--- src/main/java/com/revature/model/Medication.java
+++ src/main/java/com/revature/model/Medication.java
@@ -1,11 +1,15 @@
 package com.revature.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "medication")
 public class Medication {
     @Id
     @Column(name = "medicationid")
